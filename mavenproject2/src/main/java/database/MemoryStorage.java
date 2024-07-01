acb52df578dc6e0be36494a33f79235fc726d6ba
A-13 <A-13@A-13>
2024-07-01 09:46:40 +0700

    database

---
+++ mavenproject2/src/main/java/database/MemoryStorage.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+/**
+ *
+ * @author A-13
+ */
+public class MemoryStorage implements DataStorage{
+    private String data;
+
+    @Override
+    public void writeData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public String readData() {
+        return this.data;
+    }
+}
