cd671408d377888b120d880241b621b03e34a562
A-13 <A-13@A-13>
2024-07-01 09:31:59 +0700

    interface

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/MobilBaru.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author A-13
+ */
+class MobilBaru extends Mobil{
+    private String garansi;
+
+    public MobilBaru(String garansi, String merk, String model) {
+        super(merk, model);
+        this.garansi = garansi;
+    }
+
+    public String getGaransi() {
+        return garansi;
+    }
+
+    @Override
+    public void info() {
+        System.out.println("Mobil Baru: " + getMerk() + " " + getModel() + "(Garansi: " + garansi + ")");
+    }
+    
+}
+
